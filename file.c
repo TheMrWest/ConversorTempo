0a416ba36a28bd8cd4b140c30c5d85390c4db627
Wesley H. - MrWest <65507985+TheMrWest@users.noreply.github.com>
2024-05-26 16:51:19 -0300

    Update file.c

--- file.c
+++ file.c
@@ -4,13 +4,13 @@
 // 1 hora = 60 minutos
 
 int main() {
-	int tempo_segundos, horas, minutos, segundos;
-	scanf("%d", &tempo_segundos);
+   int tempo_segundos, horas, minutos, segundos;
+   scanf("%d", &tempo_segundos);
 	
-	horas = (tempo_segundos / 60) / 60;
-	minutos = (tempo_segundos / 60) % 60;
-	tempo_segundos %= 60;
+   horas = (tempo_segundos / 60) / 60;
+   minutos = (tempo_segundos / 60) % 60;
+   tempo_segundos %= 60;
 	
-	printf("%d:%d:%d\n", horas, minutos, tempo_segundos);
-	return 0;
+   printf("%d:%d:%d\n", horas, minutos, tempo_segundos);
+   return 0;
 }
